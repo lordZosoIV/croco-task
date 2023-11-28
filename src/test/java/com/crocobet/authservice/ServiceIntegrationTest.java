c1e41fc196d558b44b7b740f4e0c4fcb1838ade3
gmdinaradze <givi@nebuladapps.com>
2023-11-28 21:04:34 +0400

    lil fix

--- src/test/java/com/crocobet/authservice/ServiceIntegrationTest.java
+++ src/test/java/com/crocobet/authservice/ServiceIntegrationTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,13 +20,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ServiceIntegrationTest {
 
     private static final String USER_NAME = "test";
-    private static final String USER_EMAIL = "as@gmail.com";
+    private static final String USER_EMAIL = "aaaasdasfs@gmail.com";
     private static final String HAZELCAST_MAP = "user";
     private static final UserRegistrationRequest USER_REQUEST = UserRegistrationRequest.builder()
             .email("aaaasdasfs@gmail.com")
             .password("test")
             .name("test")
             .build();
+
+    private static final UserRegistrationRequest USER_REQUEST2 = UserRegistrationRequest.builder()
+            .email("faskdask@gmail.com")
+            .password("test")
+            .name("asdasdasdasd")
+            .build();
+
     @Autowired
     private AdminServiceFacade adminServiceFacade;
     @Autowired
@@ -46,5 +54,16 @@ public class ServiceIntegrationTest {
         assertEquals(USER_NAME, adminServiceFacade.getById(4L).getName());
         assertEquals(USER_EMAIL, result.getEmail());
     }
+
+
+    @Test
+    @DirtiesContext
+    public void testDeleteUser() {
+        UserInfoResponse userResponse = adminServiceFacade.create(USER_REQUEST2);
+        adminServiceFacade.deactivate(5L);
+        assertEquals(USER_REQUEST2.getName(), adminServiceFacade.getById(5L).getName());
+        assertEquals(adminServiceFacade.getById(5L).getActive(), false);
+    }
+
 
 }
